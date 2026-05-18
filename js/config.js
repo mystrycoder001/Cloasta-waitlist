26222949bad4806befb21c1eda97bb3e1e494a6d
Cloasta Deployer <deploy@cloasta.com>
2026-05-18 11:17:30 +0530

    chore: initial commit for standalone Cloasta waitlist project

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    SUPABASE_URL: 'https://ibsngqwkaasswscqnlhl.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlic25ncXdrYWFzc3dzY3FubGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTMwMTAsImV4cCI6MjA5NDMyOTAxMH0.Obb19o0RfcPfyh_R1ygowBLiUtUDr7dz38978tb9nG0'
+};
